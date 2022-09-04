250f8e41122bafc425bcbc11ea0bd5cfbc482ef5
yusenran <wired.ran@gmail.com>
2022-09-05 08:30:38 +0900

    [add]json api

--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Hello struct {
+	Greeting string `json:"greeting"`
+	Content string `json:"content"`
+	Count int `json:"count"`
+}
+
+var hello *Hello = &Hello{
+	Greeting: "Hello",
+	Content: "World",
+	Count: 0,
+}
+
 func main() {
-	http.HandleFunc("/", echoHello)
+	// http.HandleFunc("/", echoHello)
+	http.HandleFunc("/hello", helloJson)
 	http.ListenAndServe(":3000", nil)
+}
+
+func helloJson(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	switch r.Method {
+	case "GET":
+		w.Header().Set("Content-Type", "application/json")
+		j, _ := json.Marshal(hello)
+		w.Write(j)
+	case "POST":
+		d := json.NewDecoder(r.Body)
+		p := &Hello{}
+		err := d.Decode(p)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		hello = p
+		j, _ := json.Marshal(hello)
+		w.Write(j)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "unsupported")
+	}
 }
 
 func echoHello(w http.ResponseWriter, r *http.Request) {
